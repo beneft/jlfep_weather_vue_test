713148e8aa0493cfbb426d73bcfad44b69bdadcb
beneft <letun_igor007@mail.ru>
2025-07-16 13:24:12 +0500

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'jlfep_weather_vue_test',
   plugins: [
     vue(),
     vueDevTools(),
